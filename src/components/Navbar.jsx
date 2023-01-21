8cc61af9461a28349cd1ac9ad4c22c385045ce60
Faisal Bahari <fbhr1112@gmail.com>
2023-01-21 17:58:19 +0700

    add navbar on desktop mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {RxHamburgerMenu} from 'react-icons/rx'
+import DarkModeToggle from "react-dark-mode-toggle";
 
 const Navbar = () => {
-
-  window.onscroll = function(){
-    const header = document.getElementById('header');
-    const fix = header.offsetTop;
-
-    if(window.pageYOffset > fix){
-      header.classList.add('header-fix');
-    } else{
-      header.classList.remove('header-fix');
-    }
-  }
-
+  const [isDarkMode, setIsDarkMode] = useState(() => true);
   return (
     <div className='fixed w-full bg-[rgba(0,30,38,.6)] backdrop-blur-md z-10'>  
           <div className='relative flex items-center justify-between py-4 px-6 max-w-7xl mx-auto'>
             <div>
                 <img className='h-8 md:h-10' src="https://hmifunej.id/images/HMIF%20WORDMARK%20LOGO.png" alt="" />
             </div>
-            <div className='text-white scale-125 -mt-2 cursor-pointer'>
+            <div>
+              <ul className='hidden md:flex gap-4 text-sm font-semibold -mt-1'>
+                <li>Beranda</li>
+                <li>Tentang</li>
+                <li>Staf</li>
+                <li>Proker</li>
+                <li>Blog</li>
+                <li>Portofolio</li>
+              </ul>
+            </div>
+            <div className='hidden md:block'>
+              <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={50}/>
+            </div>
+            <div className='text-white scale-125 -mt-2 cursor-pointer md:hidden'>
                 <RxHamburgerMenu/>
             </div>
         </div>
