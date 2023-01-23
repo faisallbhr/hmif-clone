36afdc930b9b5107673b9f40189bce36963ca8b0
Faisal Bahari <fbhr1112@gmail.com>
2023-01-23 23:56:05 +0700

    edit not found page

--- src/pages/Capek.jsx
+++ src/pages/Capek.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Picture from '../assets/projectnf.png'
 
 const Capek = () => {
   return (
     <div>
       <Navbar/>
       <div className='h-screen flex justify-center items-center'>
-          <h1 className='font-bold text-7xl text-center'>KAPAN-KAPAN AJA</h1>
+          <img src={Picture} alt="" />
       </div>
     </div>
   )
