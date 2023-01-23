77260189d4a9c0d43cde0d11872d3f852e0a09ec
Faisal Bahari <fbhr1112@gmail.com>
2023-01-23 21:12:38 +0700

    add loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import 'swiper/css/bundle';
@@ -6,11 +6,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Aboutpage from "./pages/Aboutpage";
 import Capek from "./pages/Capek";
+import { BounceLoader } from "react-spinners";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500);
+  }, [])
   return (
     <div className="App bg-[rgb(31,41,55)] text-white">
-      <BrowserRouter>
+      {loading? 
+      <div className='bg-[#ebebeb] flex items-center justify-center h-screen'>
+        <BounceLoader color='rgb(16,185,129)' loading={loading}/>
+      </div> : 
+        <BrowserRouter>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Homepage/> }></Route>
@@ -19,6 +31,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
+      }
     </div>
   );
 }
